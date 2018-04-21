6a796d688b632a9a9e6334c262cae21169c5becf
Rida CCNA <ridz_1208@hotmail.com>
2018-04-21 09:04:40 -0400

    Added

--- tables_sql/Meta/medical_history_meta.sql
+++ tables_sql/Meta/medical_history_meta.sql
@@ -4,4 +4,8 @@ INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_num
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES('medical_history', 'medical_history_page2', 'Page 2', 2);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES('medical_history', 'medical_history_page3', 'Page 3', 3);
 
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('medical_history', 1, 99999, 'Y', 'Visit', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('medical_history', 1, 99999, 'Y', 'Visit', NULL, 'V1', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('medical_history', 1, 99999, 'Y', 'Visit', NULL, 'V2', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('medical_history', 1, 99999, 'Y', 'Visit', NULL, 'V4', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('medical_history', 1, 99999, 'Y', 'Visit', NULL, 'V5', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('medical_history', 1, 99999, 'Y', 'Visit', NULL, 'V6', NULL, NULL, NULL);
