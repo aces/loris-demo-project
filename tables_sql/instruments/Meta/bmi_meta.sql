67ae1010cd0d9aa8eed70b03614a2edda0a30a54
ridz1208 <ridz_1208@hotmail.com>
2019-02-09 11:52:50 -0500

    cleanup

--- tables_sql/instruments/Meta/bmi_meta.sql
+++ tables_sql/instruments/Meta/bmi_meta.sql
@@ -1,9 +1,15 @@
-INSERT INTO test_names (Test_name, Full_name, Sub_group) VALUES('bmi', 'BMI Calculator', 1);
+INSERT INTO test_names (Test_name, Full_name, Sub_group) SELECT 'bmi', 'BMI Calculator', ID FROM test_subgroups WHERE Subgroup_name = 'Instruments';
 
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('bmi', 1, 2147483647, 'Y', 'Visit', NULL, 'V1', NULL, NULL, 1);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('bmi', 1, 2147483647, 'Y', 'Visit', NULL, 'V2', NULL, NULL, 1);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('bmi', 1, 2147483647, 'Y', 'Visit', NULL, 'V3', NULL, NULL, 1);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('bmi', 1, 2147483647, 'Y', 'Visit', NULL, 'V4', NULL, NULL, 1);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('bmi', 1, 2147483647, 'Y', 'Visit', NULL, 'V5', NULL, NULL, 1);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('bmi', 1, 2147483647, 'Y', 'Visit', NULL, 'V6', NULL, NULL, 1);
 
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V1' FROM subproject WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V2' FROM subproject WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V3' FROM subproject WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V4' FROM subproject WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V5' FROM subproject WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V6' FROM subproject WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V1' FROM subproject WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V2' FROM subproject WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V3' FROM subproject WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V4' FROM subproject WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V5' FROM subproject WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label) SELECT 'bmi', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V6' FROM subproject WHERE title = 'Stale';
--- tables_sql/instruments/Meta/radiology_review_meta.sql
+++ tables_sql/instruments/Meta/radiology_review_meta.sql
@@ -1,7 +1,10 @@
-INSERT INTO test_names (Test_name, Full_name, Sub_group) VALUES ('radiology_review', 'Radiology review', '1');
+INSERT INTO test_names (Test_name, Full_name, Sub_group) SELECT 'radiology_review', 'Radiology review', ID FROM test_subgroups WHERE Subgroup_name = 'Imaging';
 
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('radiology_review', 0, 2147483647, 'Y', 'Visit', 1, 'V1', 1, NULL, NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('radiology_review', 0, 2147483647, 'Y', 'Visit', 2, 'V1', 2, NULL, NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('radiology_review', 0, 2147483647, 'Y', 'Visit', 1, 'V2', 1, NULL, NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('radiology_review', 0, 2147483647, 'Y', 'Visit', 2, 'V2', 2, NULL, NULL);
-
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label)
+  SELECT 'radiology_review', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V1' FROM subproject WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label)
+  SELECT 'radiology_review', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V1' FROM subproject WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label)
+  SELECT 'radiology_review', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V2' FROM subproject WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label)
+  SELECT 'radiology_review', 1, 2147483647, 'Y', 'Visit', SubprojectID, 'V2' FROM subproject WHERE title = 'Fresh';
